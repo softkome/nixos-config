6a48f9e4cc221085125fb9af5a480935f22b36f1
softkome <softkome+git@gmail.com>
2025-08-06 23:32:18 +0100

    updated fastfetch.nix

--- home-manager/modules/fastfetch.nix
+++ home-manager/modules/fastfetch.nix
@@ -1,43 +1,39 @@
 { pkgs, lib, config, ... }: {
-
   options = {
     fastfetchMod = lib.mkEnableOption "enables fastfetchMod";
   };
-
   config = lib.mkIf config.fastfetchMod {
     programs.fastfetch = {
       enable = true;
       settings = {
         logo = {
-	  type = "raw";
-	  source = ./assets/fastfetchlogo.txt;
-	  width = 26;
-          height = 6;
+          type = "raw";
+          source = ./assets/fastfetchlogo.txt;
+          width = 12;
+          height = 4;
           padding = {
-            top = 0;
+            top = 1;
             left = 2;
-            right = 6;
-	  };
-	};
-
-	modules = [
-	  {
-	    type = "os";
-	    key = " OS";
-	  }
-	  {
-	    type = "packages";
-	    key = " Packages";
-	  }
-	  {
-	    type = "uptime";
-	    key = "󰞌 Uptime";
-	  }
-	  {
-	    type = "shell";
-	    key = " Shell";
-	  }
-	];
+          };
+        };
+        modules = [
+          {
+            type = "os";
+            key = " OS";
+          }
+          {
+            type = "packages";
+            key = " Packages";
+          }
+          {
+            type = "uptime";
+            key = "󰞌 Uptime";
+          }
+          {
+            type = "shell";
+            key = " Shell";
+          }
+        ];
       };
     };
   };
