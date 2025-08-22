e5e982cd6fc6afea6ac843b0d2638995744fec8a
softkome <softkome+git@gmail.com>
2025-08-22 02:36:52 +0100

    added templates

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -1,14 +1,24 @@
 { lib, config, osConfig, ... }:
 let
   cfg = config.components.git;
-  secrets = import ../../secrets.nix;
 in
-  {
-    config = lib.mkIf cfg {
-      programs.git = {
-        enable = true;
-	userName = builtins.readFile osConfig.sops.secrets."git/userName".path;
-	userEmail = builtins.readFile osConfig.sops.secrets."git/userEmail".path;
+{
+  config = lib.mkIf cfg {
+    programs.git = {
+      enable = true;
+      
+      # Use the template created by sops-nix instead of reading files directly
+      includes = [{
+        path = osConfig.sops.templates."git-config".path;
+      }];
+      
+      # Optional: Add other git configuration
+      extraConfig = {
+        init.defaultBranch = "main";
+        pull.rebase = false;
+        push.default = "simple";
+        core.autocrlf = "input";
       };
     };
-  }
+  };
+}
--- modules/system/sops.nix
+++ modules/system/sops.nix
@@ -1,23 +1,25 @@
-{ user, ...}: {
+{ user, config, ... }: {
   sops = {
     defaultSopsFile = ../../secrets/secrets.yaml;
     defaultSopsFormat = "yaml";
-
     age = {
       keyFile = "/home/${user}/.config/sops/age/keys.txt";
     };
-
     secrets = {
-      "git/userName" = {
-        owner = user;
-	group = "users";
-	mode = "0400";
-      };
-      "git/userEmail" = {
-        owner = user;
-	group = "users";
-	mode = "0400";
-      };
+      "git/userName" = {};
+      "git/userEmail" = {};
+    };
+    
+    # Add this template
+    templates."git-config" = {
+      content = ''
+        [user]
+          name = ${config.sops.placeholder."git/userName"}
+          email = ${config.sops.placeholder."git/userEmail"}
+      '';
+      owner = user;
+      group = "users";
+      mode = "0400";
     };
   };
 }
