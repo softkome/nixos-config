3135237607079d3c46e9fa2bd1cbb1c6ecb83524
softkome <softkome+git@gmail.com>
2025-11-20 17:23:57 +0000

    removed fastfetch logo

--- modules/home-manager/foot.nix
+++ modules/home-manager/foot.nix
@@ -6,8 +6,10 @@ in
   config = lib.mkIf cfg {
     programs.foot = {
       enable = true;
-
       settings = {
+        main = {
+	  font = lib.mkForce "JetBrains Mono:size=11";
+	};
         scrollback = {
           lines = 100;
         };
