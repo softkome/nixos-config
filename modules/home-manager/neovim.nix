5274974b60ecbc99a18beef5aa1a3cc5cd3a0acb
softkome <softkome+git@gmail.com>
2025-11-23 12:58:54 +0000

    added nvim-autopairs and nvim-surround

--- modules/home-manager/neovim.nix
+++ modules/home-manager/neovim.nix
@@ -6,6 +6,15 @@ in
     config = lib.mkIf cfg {
       programs.neovim = {
         enable = true;
+	plugins = with pkgs.vimPlugins; [
+	  nvim-autopairs
+	  nvim-surround
+	];
+	extraLuaConfig = ''
+	  require("nvim-autopairs").setup {}
+	  require("nvim-surround").setup {}
+	  vim.opt.number = true
+	'';
 	extraPackages = with pkgs; [
 	  vimPlugins.nvim-treesitter-parsers.hyprlang
 	];
