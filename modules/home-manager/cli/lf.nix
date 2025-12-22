9e811d0fb32f7b2c6d60988cff056460f1adb5c8
softkome <softkome+git@gmail.com>
2025-12-22 13:55:11 +0000

    lf rename keybind

--- modules/home-manager/cli/lf.nix
+++ modules/home-manager/cli/lf.nix
@@ -9,6 +9,20 @@ in {
   config = lib.mkIf cfg.enable {
     programs.lf = {
       enable = true;
+
+      commands = {
+        rename = ''
+          %{{{
+                   printf "Rename '%s' to: " "$f"
+                   read new
+                   [ -n "$new" ] && mv -- "$f" "$(dirname "$f")/$new"
+                 }}}
+        '';
+      };
+
+      keybindings = {
+        r = "rename";
+      };
     };
   };
 }
