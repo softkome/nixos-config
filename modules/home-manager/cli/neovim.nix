70404bd9169e60382de85d4a65899a48bdd5024b
softkome <softkome+git@gmail.com>
2026-01-04 20:52:56 +0000

    nvim-colorizer

--- modules/home-manager/cli/neovim.nix
+++ modules/home-manager/cli/neovim.nix
@@ -15,6 +15,11 @@ in {
       ];
       extraLuaConfig = ''
                require("nvim-autopairs").setup {}
+	       require("colorizer").setup({
+                 "*"     = { RGB = true; RRGGBB = true; }; -- all filetypes
+                 css     = { css = true; };                -- css support
+                 html    = { names = false; };             -- html tweaks
+               })
                require("nvim-surround").setup {}
         vim.opt.number = true
       '';
