8802756a8cb9bd31e078f71e2ab7c1b875e17df8
softkome <softkome+git@gmail.com>
2026-01-04 18:03:46 +0000

    new colour scheme

--- modules/home-manager/stylix.nix
+++ modules/home-manager/stylix.nix
@@ -24,7 +24,7 @@ in {
     stylix = {
       enable = true;
       polarity = "dark";
-      base16Scheme = "${assetsDir}/colours/gruvbox.yaml";
+      base16Scheme = "${assetsDir}/colours/custom.yaml";
 
       targets = {
         neovim.enable = true;
